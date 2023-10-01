fa87044244dcbccd48b88d1ed767ae2cc769f9c8
Dharmendra kumar <dharm117x@gmail.com>
2023-10-01 11:53:40 +0530

    update command

--- script/snap_remove.sh
+++ script/snap_remove.sh
@@ -2,7 +2,7 @@
 # remove old snaps
 
 set -eu
-snap list --all | awk `/disabled/{print $1, $3}` |
+snap list --all | awk '/disabled/{print $1, $3}' |
 	while read snapname revision; do
 		snap remove "$snapname" --revision="$revision"
 	done
